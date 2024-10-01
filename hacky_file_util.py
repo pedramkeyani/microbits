6f2d0da856cfa0bd1c20eceff1c88fe9e63187ef
Pedram Keyani <pedram@gmail.com>
2024-10-01 11:00:25 -0700

    Update hacky_file_util.py
    Improved documentation and moved the test code below the helper functions.

--- hacky_file_util.py
+++ hacky_file_util.py
@@ -1,4 +1,15 @@
-# Imports go at the top
+# -----------------------------------------------------------------------------
+# Author: Pedram Keyani
+# Email: pedram@openai.com
+# Date: 2024-10-01
+#
+# Description: 
+# The microbits python file documentation notes that the micro python implentation
+# of files is very limited. In order to append, you must first read the contents,
+# add your new content to that list and then overwrite the file. The helper functions
+# file_exists, list_files, read_file and append_file can just be cut/paste into other
+# projects
+# -----------------------------------------------------------------------------
 from microbit import *
 from os import *
 
@@ -10,48 +21,49 @@ def file_exists(file_name) -> bool:
     return False
 
 
-
 def list_files():
     files = listdir()
     print(files)
 
-
-list_files()
-
-
-def read_file() -> str:
-    with open('test.txt', 'r') as file:
+def read_file(file_name) -> str:
+    with open(file_name, 'r') as file:
         lines = file.read()
         return lines
 
-def append_file(new_line):
-    if file_exists('test.txt') == False:
-        open('test.txt', 'w')
+def append_file(file_name, new_line):
+    if file_exists(file_name) == False:
+        open(file_name, 'w')
     lines = ''
-    with open('test.txt', 'r') as file:
+    with open(file_name, 'r') as file:
         lines = file.read()
    
-    with open('test.txt', 'w') as file:
+    with open(file_name, 'w') as file:
         lines = lines + '\n' + new_line
         file.write(lines)
 
-print(file_exists('test.txt'))
+# Testing the functions above
 
-append_file('a')
-append_file('b')
-append_file('c')
+list_files()
 
-print(file_exists('test.txt'))
+file_name =  'test.txt'
 
-lines = read_file()
+print(file_exists(file_name))
+
+append_file(file_name, 'a')
+append_file(file_name, 'b')
+append_file(file_name, 'c')
+
+print(file_exists(file_name))
+
+lines = read_file(file_name)
 print(lines)
 
-append_file('d')
-lines = read_file()
+append_file(file_name, 'd')
+lines = read_file(file_name)
 print(lines)
 
-append_file('e')
-lines = read_file()
+append_file(file_name, 'e')
+lines = read_file(file_name)
 print(lines)
 
 list_files()
